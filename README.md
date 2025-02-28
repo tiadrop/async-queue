36a5be8f2d06ea8d9b8ab744d99741110831af00
xtaltia <aletabix@pm.me>
2025-02-28 02:36:39 +0000

    init

---
+++ README.md
@@ -0,0 +1,83 @@
+# async-queue
+
+A lightweight but powerful asynchronous task queuing system, with smart task priority and configurable concurrency limit.
+
+## Basic Use:
+
+`npm i @xtia/async-queue`
+
+```ts
+import { AsyncQueue } from "@xtia/async-queue";
+
+// create a queue with default options
+// (maxConcurrent = 1)
+const queue = new AsyncQueue();
+
+// enqueue a task with a Promise-like interface:
+// (enqueue() returns a promise that forwards the task's resolution)
+const text = await queue.enqueue<Response>(resolve => {
+    fetch(someUrl).then(resolve);
+}).then(r => r.text());
+
+// enqueue a task with a specific priority:
+queue.enqueue(resolve => {
+    someAsyncOperation().then(resolve);
+}, 1);
+```
+
+As well as resolution, promise rejection is forwarded by `enqueue`:
+```ts
+queue.enqueue((resolve, reject) => {
+    fetch("doesntexist.lol").then(resolve, reject);
+}).catch((err) => console.warn("fetch failed:", err));
+```
+
+## `queue.createFunc()`
+
+`createFunc(fn, priority?)` lets us easily convert asynchronous functions to identically-signed functions that place their behaviour in the queue.
+
+```ts
+const queue = new AsyncQueue({
+    maxConcurrent: 3, // allow 3 notifications to be shown at a time
+});
+
+// we have a normal function to show a notification
+function showNotification(level: "info" | "warning", message: string) {
+    return new Promise(resolve => {
+        // ... code to show notification
+        // resolve when removed by click or timeout
+    });
+}
+
+// wrap it so that its behaviour is queued when called
+export const enqueueNotification = queue.createFunc(showNotification);
+
+// function's signature is maintained, but calls are automatically enqueued:
+enqueueNotification("info", "Download complete");
+```
+
+When wrapping the function, we can provide a priority for that function, or a function to determine a call's priority from its arguments:
+
+```ts
+// prioritise warnings
+export const enqueueNotification = queue.createFunc(
+    showNotification,
+    (level, message) => level == "warning" ? 1 : 2
+);
+```
+
+## Constructor options
+
+`AsyncQueue`'s constructor can be passed an object with the following properties
+
+### `maxConcurrent: number`
+
+Specifies how many tasks can be processed at a time. Default is 1.
+
+### `defaultPriority: number`
+
+Specifies a standard priority. Default is 5.
+
+### `delayMs: number`
+
+Specifies a delay between a queued task's completion and its concurrency slot becoming available for another task. Default is 0.
---
+++ src/index.ts
@@ -0,0 +1,93 @@
+type Resolver<T> = (resolve: (value: T) => void, reject: (reason: any) => void) => void;
+type QueueTask<T> = {
+	priority: number;
+	func: Resolver<T>;
+	resolve: (value: T) => void;
+	reject: (reason: any) => void;
+}
+
+type AsyncQueueOptions = {
+	maxConcurrent?: number;
+	defaultPriority?: number;
+	delayMs?: number;
+}
+
+export class AsyncQueue {
+	private queue: QueueTask<any>[] = [];
+	private busyTasks: number = 0;
+	private priorityCounts: Map<number, number> = new Map();
+	private maxConcurrent: number;
+	private defaultPriority: number;
+	private delayMs: number;
+
+	constructor(options?: AsyncQueueOptions) {
+		this.maxConcurrent = options?.maxConcurrent ?? 1;
+		this.defaultPriority = options?.defaultPriority ?? 5;
+		this.delayMs = options?.delayMs ?? 0;
+	}
+
+	private adjustPriorityCount(priority: number, delta: number) {
+		const value = (this.priorityCounts.get(priority) ?? 0) + delta;
+		if (value == 0) {
+			this.priorityCounts.delete(priority);
+		} else {
+			this.priorityCounts.set(priority, value);
+		}
+	}
+
+	private getInsertionIndex(priority: number) {
+		// find all present priorities less than or equal to given:
+		const ltePriorities = [...this.priorityCounts.keys()].filter(k => k <= priority);
+		// insertion point is sum of their counts:
+		return ltePriorities.reduce((acc, p) => acc + (this.priorityCounts.get(p) as number), 0);
+	}
+
+	private continue() {
+		while (this.busyTasks < this.maxConcurrent) {
+			if (this.queue.length == 0) return;
+
+			this.busyTasks++;
+			const task = this.queue.shift() as QueueTask<any>;
+			this.adjustPriorityCount(task.priority, -1);
+
+			const result = new Promise(task.func);
+
+			result.then(
+				value => task.resolve(value),
+				value => task.reject(value)
+			).finally(() => setTimeout(() => {
+				this.busyTasks--;
+				this.continue();			
+			}, this.delayMs));
+		}
+	}
+
+	enqueue<T>(func: Resolver<T>, priority: number = this.defaultPriority) {
+		return new Promise<T>((resolve, reject) => {
+			const task = { func, priority, resolve, reject };
+
+			const insertIdx = this.getInsertionIndex(priority);
+			this.adjustPriorityCount(priority, 1);
+
+			this.queue.splice(insertIdx, 0, task);
+			this.continue();
+		});
+	}
+
+	createFunc<F extends (...args: any[]) => Promise<any>>(
+		fn: F,
+		priority: number | ((...args: Parameters<F>) => number) = this.defaultPriority
+	) {
+        type A = Parameters<F>;
+        type R = ReturnType<F> extends Promise<infer T> ? T : never;
+        return typeof priority == "function"
+            ? (...args: A) => this.enqueue<R>(
+                (resolve, reject) => fn(...args).then(resolve, reject),
+                priority(...args)
+            )
+            : (...args: A) => this.enqueue<R>(
+                (resolve, reject) => fn(...args).then(resolve, reject),
+                priority
+            );
+	}
+}
